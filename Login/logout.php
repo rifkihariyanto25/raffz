009de40338a16704a8fc2582da64185549518b07
rifkihariyanto25 <hariyantorifki25@gmail.com>
2024-12-09 18:06:09 +0700

    first commit

--- Login/logout.php
+++ Login/logout.php
@@ -4,9 +4,6 @@ session_start();
 require_once 'vendor/autoload.php';
 $access_token = $_SESSION['acces_token'];
 
-// inisiasi google client
-$client = new Google_Client();
-
 $client->revokeToken();
 session_destroy();
 header('location: login.php');
